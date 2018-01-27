2c66b78da48c5564e3043a50369c1d6d269a6826
bristlecone <hangbok@hotmail.com>
2018-01-27 22:37:51 +0900

    '2'

--- app.js
+++ app.js
@@ -5,31 +5,9 @@ app.set('view engine', 'pug');
 //생략 가능
 app.set('views', './views');
 app.use(express.static('public')); //static directory 'public'
-app.get('/topic', function(req, res){
-    res.send(req.query.id);
-})
-app.get('/template', function(req, res){
-    res.render('temp',{time:Date(), _title:'HJ'});
-})
 app.get('/', function(req, res){
     res.send('welcome to home');
 });
-app.get('/login', function(req, res){
-    res.send('login please');
-})
-app.get('/route', function(req, res){
-    res.send('test.html')
-})
-app.get('/dynamic', function(req, res){
-    var lis = '';
-    for(var i = 0; i < 5; i++)
-    {
-        lis += '<li>coding</li>';
-    }
-    var time = Date();
-    var output = `Hello, dynamic!${lis} ${time}`
-    res.send(output);
-})
-app.listen(3000, function(){
+app.listen(80, function(){
     console.log('Connected 3000 port');
 });
