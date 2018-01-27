d885e7fab9eb2d0c092f30c52be74c57702b8ead
bristlecone <hangbok@hotmail.com>
2018-01-28 02:16:05 +0900

    css import

--- app.js
+++ app.js
@@ -5,9 +5,10 @@ var port = process.env.PORT || 5000;
 app.locals.pretty = true;
 //생략 가능
 app.set('views', './views');
+app.use('view engine', 'pug');
 app.use(express.static('public')); //static directory 'public'
 app.get('/', function(req, res){
-    res.send('welcome to home');
+    res.render('main');
 });
 app.listen(port, function(){
     //console.log('Connected 3000 port');
