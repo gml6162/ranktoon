ff335c761814853b03c05d34dc14bdb39318888b
bristlecone <hangbok@hotmail.com>
2018-01-28 02:32:07 +0900

    view engine setting

--- app.js
+++ app.js
@@ -4,12 +4,12 @@ var port = process.env.PORT || 5000;
 
 app.locals.pretty = true;
 //생략 가능
+app.set('view engine', 'pug');
 app.set('views', './views');
-app.use('view engine', 'pug');
 app.use(express.static('public')); //static directory 'public'
 app.get('/', function(req, res){
     res.render('main');
 });
 app.listen(port, function(){
-    //console.log('Connected 3000 port');
+    console.log('Connected 3000 port');
 });
